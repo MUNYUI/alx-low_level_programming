8eb58c32cfbec39b26d0e4f86a3462f626ec3c3e
Munyui <munyuinloise@gmail.com>
2022-09-10 11:11:18 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,40 @@
 #!/bin/bash
 #include <stdio.h>
+
+
+
+/**
+ *
+ *  *  * main - Entry point
+ *
+ *   *
+ *
+ *    *   *
+ *
+ *     *
+ *
+ *      *    * Return: Alwats 0 (Success)
+ *
+ *       *
+ *
+ *        */
+
+
+
 int main(void)
+
+
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-} 
+
+
+
+			puts("\"Programming is like building a multilingual puzzle");
+
+
+
+						return (0);
+
+
+
+}
