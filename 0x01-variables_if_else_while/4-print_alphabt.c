dad17bc15e7ba464d3d742e756be81f04860b239
Munyui <munyuinloise@gmail.com>
2022-09-13 14:18:01 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - prints the alphabets in lower case only
+ * excludes q and e and adds a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+	)
+
