0faf42c0cbef1941cf25d9c92acd9f93f93e6964
Munyui <munyuinloise@gmail.com>
2022-09-13 11:51:42 -0700

    Allphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - reads characters in the alphabet
+ * print results in the correct order in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
