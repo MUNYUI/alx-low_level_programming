76e4ba6f7031672fe171540a2db26c8ff6099a1d
Munyui <munyuinloise@gmail.com>
2022-09-14 13:35:50 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,7 @@
  * print_alphabet - Prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
+ *
  */
 
 void print_alphabet(void)
@@ -14,5 +15,5 @@ void print_alphabet(void)
 	{
 		putchar(alpha);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
