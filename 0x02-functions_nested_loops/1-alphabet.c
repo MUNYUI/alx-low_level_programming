a245d8e7a95194ac180e6378b2604d91ca982416
Munyui <munyuinloise@gmail.com>
2022-09-16 01:32:54 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "main.h"
+
 /**
  * print_alphabet - Prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
- *
  */
-
 void print_alphabet(void)
-
 {
 	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		putchar(alpha);
+		_putchar(alpha);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
