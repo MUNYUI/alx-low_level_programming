13a345d13acfc17e36e5bdec47fbf32e05efcc77
Munyui <munyuinloise@gmail.com>
2022-09-16 00:34:23 -0700

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,5 +2,16 @@
 
 /**
  * main - print letters of the alphabet in reverse
- * eturn: Always 0 (Success)
- * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
