28f8400fbef56a356ac14203d3698820db338f13
Munyui <munyuinloise@gmail.com>
2022-09-26 16:19:43 -0700

    Task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,14 +2,19 @@
 
 /**
  * set_string - Sets the value of a pointer to char
+ *
  * @s: the value to modify
- * @to: the value to assign
+ *
+ *  @to: the value to assign
  *
  * Return: Nothing.
+ *
  */
 
 void set_string(char **s, char *to)
 
 {
+
 	*s = to;
+
 }
