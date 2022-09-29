a0af7e9c344c2b42c4e0f227a4ecc45ea1d08129
Munyui <munyuinloise@gmail.com>
2022-09-29 06:20:26 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,24 +3,17 @@
 #include <stdlib.h>
 
 /**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- * Return: 1 if charcter is uppercase, 0 otherwise.
+ *  _isupper - check for upper case letter
+ *  @c : character to check the case
+ * Return:0 or 1
  */
 
 int _isupper(int c)
-
 {
-	if ((c >= 'A') && (c <= 'Z'))
-
+	if (isupper(c))
 	{
-
 		return (1);
-
 	}
-
 	else
-
 		return (0);
-
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,9 +12,8 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_square (int size);
+void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
-void print_number(int n)
+void print_number(int n);
 
-#endif	
+#endif /* MAIN_H */
