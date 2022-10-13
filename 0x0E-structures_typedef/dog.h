ed4205ffa60064c500b2db872c0c0b9c083733d0
Munyui <munyuinloise@gmail.com>
2022-10-13 03:44:30 -0700

    Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef FILE_DOG
-#define FILE_DOG
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - structure dog
@@ -13,7 +13,10 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+/**
+ * dog_t - Typedef for struct dog
+ */
 
 typedef struct dog dog_t;
 
@@ -23,6 +26,8 @@ void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
 
-void free_dog(dog_t *d);
+void free_dogi(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
